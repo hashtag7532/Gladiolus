d31c5bd8e7586d2c3699e87cff40a3dd139074bf
Parth Dodia <hashtag7532@gmail.com>
2023-09-17 01:47:27 +0530

    Index page created, about us mobile and desktop done

--- pages/data.js
+++ pages/data.js
@@ -9,7 +9,7 @@ export default [
         img: "/images/2.jpg",
         title: "Complete portfolio website tutorial",
         text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-        link : "/images/Acceptance Letter.pdf",
+        link : "/images/Application Form.pdf",
     },
     {
         img: "/images/3.jpg",
